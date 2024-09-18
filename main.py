efc0cdc89a79d9252afeb165d1daddef9d333d61
iaPlotnikovv <idoffix@gmail.com>
2024-09-18 23:32:59 +0300

    added player

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
+    fps= pygame.time.Clock()
+    dt = 0
+
     while True:
          for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-         open_window(screen)
+         game_window(screen,player)
+         dt=fps.tick(60)/1000
+        
 
-def open_window(screen):
-        screen.fill("black")
+def game_window(screen, player):
+        screen.fill("purple")
+        player.draw(screen)
         pygame.display.flip()
 
 #This line ensures the main() function is only called when this file is run directly; it won't run if it's imported as a module.
